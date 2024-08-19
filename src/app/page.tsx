c406a55e72fae1055249dc8b2e9c2da5f7179d08
imaimai17468 <tosiki_801_life@outlook.jp>
2024-08-20 01:51:36 +0900

    feat: shadcn/ui環境に塗り替え

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/Button';
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <Button>Get Started!</Button>
-    </div>
+    </main>
   );
 }
